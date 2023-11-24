f78a45c8e0974ae3605ac9e459a4e321a1d3d8c8
RichardGG99 <135170630+RichardGG99@users.noreply.github.com>
2023-11-23 18:23:52 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let nombre = prompt("Por favor ingresa tu nombre")
+
+alert("Buen dia Caballero " + nombre)
+
+alert("Que Porcentaje Hara El Dia De Hoy?")
+
+console.log("Cesarin Huele Como A Viejito jajaj")
+
+
+
+
+
+
